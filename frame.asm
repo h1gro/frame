b27aab5e9884d58e89044b13a5660c8a16ff5264
h1gro <komarov.aa@phystech.edu>
2025-02-10 00:07:22 +0300

    full frame but with out variables

--- frame.asm
+++ frame.asm
@@ -5,16 +5,28 @@ org 100h
 Main:		mov bx, 0b800h
 		mov es, bx
 		mov bx, 0
-			
-		call DrawFrame		
+
+		call DrawFrame
 
 		mov ax, 4c00h
 		int 21h
 
-DrawFrame:	mov dl, 0DAh
+DrawFrame:	call DrawLineUp
+
+		add bx, 96
+
+		mov cx, 10
+
+		call DrawVert
+
+		call DrawLineDown
+
+		ret
+
+DrawLineUp:	mov dl, 0DAh
 		mov cx, 1h
 		call DrawChar
-		
+
 		mov dl, 0C4h
 		mov cx, 30
 		call DrawChar
@@ -22,33 +34,47 @@ DrawFrame:	mov dl, 0DAh
 		mov dl, 0BFh
 		mov cx, 1h
 		call DrawChar
-	
-		call new_line
 
-		mov dl, 41h
-		mov cx, 1h
-		call DrawChar
 		ret
 
+DrawLineDown:	mov dl, 0C0h
+		mov cx, 1h
+		call DrawChar
+
+		mov dl, 0C4h
+		mov cx, 30
+		call DrawChar
+
+		mov dl, 0D9h
+		mov cx, 1h
+		call DrawChar
+
+		ret
 
 DrawChar:	mov byte ptr es:[bx], dl
 		mov byte ptr es:[bx+1], 00011111b
-		
+
 		add bx, 2h
-		
+
 		loop DrawChar
 		ret
 
-New_Line:	push ax
-		mov ah, 0Eh
-		mov al, 0Ah
-		int 10h
+DrawVert:	mov ax, cx
 
-		mov ah, 0Eh
-		mov al, 0Dh
-		int 10h
+		mov dl, 0B3h
+		mov cx, 1
+		call DrawChar
 
-		pop ax
+		add bx, 60
+
+		mov dl, 0B3h
+		mov cx, 1
+		call DrawChar
+
+		add bx, 96
+
+		mov cx, ax
+		loop DrawVert
 		ret
 
 end		Main
